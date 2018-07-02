2c0d20dfd97ba62c8895c8031fa0c1e7c8b189b2
binayag1 <binaya.g1@gmail.com>
2018-07-02 17:23:06 +1000

    Project Setup

---
+++ ecommercebackend/src/main/java/net/binaya/ecommercebackend/App.java
@@ -0,0 +1,13 @@
+package net.binaya.ecommercebackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
